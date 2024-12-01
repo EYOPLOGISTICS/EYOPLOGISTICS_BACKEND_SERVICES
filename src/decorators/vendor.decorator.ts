63c7ed608fc9ea0b4f311798d47c44c5e96a1b32
benalfred <59826097+benalfred@users.noreply.github.com>
2024-12-01 07:46:02 +0100

    Update vendor.decorator.ts

--- src/decorators/vendor.decorator.ts
+++ src/decorators/vendor.decorator.ts
@@ -7,7 +7,7 @@ export const GetVendor = createParamDecorator(
     async (data: unknown, ctx: ExecutionContext) => {
         const request = ctx.switchToHttp().getRequest();
         const vendorId = request.headers['logged-in-vendor']
-        if (vendorId) returnErrorResponse('Vendor is required')
+        if (!vendorId) returnErrorResponse('Vendor is required')
         const vendor = await Vendor.findOne({where: {id: vendorId}});
         if (!vendor) returnErrorResponse("Invalid vendor");
         return vendor;
